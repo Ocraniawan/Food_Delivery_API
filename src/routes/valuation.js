de0185db5984e390908a324f8c0522de2a4ea2fd
ocraniawan <ocraniawanpatattan@gmail.com>
2020-02-08 01:16:05 +0800

    fixed review

--- src/routes/valuation.js
+++ src/routes/valuation.js
@@ -11,16 +11,19 @@ router.post('/',(req,res)=>{
     const updated_on = new Date()
     const sql = `INSERT INTO valuation (rating,review,user_id,item_id,created_on,updated_on) VALUES (?,?,?,?,?,?)`
     mysql.execute(sql, [rating,review,user_id,item_id,created_on,updated_on], (err, result0, field)=>{
-        res.send({success: true, data: result0})
+        // res.send({success: true, data: result0})
+        console.log(err)
         if(err == null){
             const sql = `SELECT AVG(rating) AS rate from valuation WHERE item_id = ${item_id}`
-            mysql.execute(sql, [], (err, result, field)=>{
+            mysql.execute(sql, [], (err1, result, field)=>{
                 var rating = result[0].rate
                 // console.log(user_id)
-                if(err == null){
+                console.log(err1)
+                if(err1 == null){
                     const sql = `UPDATE item set rating = ${result[0].rate} WHERE id_item = ${item_id} `
-                    mysql.execute(sql, [rating], (err, result1, field)=>{
-                        if(err == null){
+                    mysql.execute(sql, [rating], (err2, result1, field)=>{
+                        console.log(err2)
+                        if(err2 == null){
                             const sql = `DELETE from carts WHERE item_id = ${item_id} AND user_id = ${user_id}`
                             mysql.execute(sql, [item_id, user_id], (err, result2, field) =>{
                                 res.send({success: true, result2})
