2f165e154bcf9b722c397b4e6af5f648dab63b22
ocraniawan <ocraniawanpatattan@gmail.com>
2020-02-08 19:31:03 +0800

    fixed review

--- src/model/valuation.js
+++ src/model/valuation.js
@@ -1,5 +1,5 @@
 const add = 'INSERT INTO valuation (rating,review,user_id,item_id,created_on,updated_on)'
-const detail = `SELECT valuation.valuation_id, item.item_name, valuation.rating, valuation.review, item.price,  user.username FROM valuation 
+const detail = `SELECT valuation.valuation_id, item.item_name, valuation.rating, valuation.review, item.price, user.name, user.username FROM valuation 
 INNER JOIN item ON valuation.item_id = item.id_item
 INNER JOIN user ON valuation.user_id = user.id
  WHERE item_id=? order by valuation_id ASC LIMIT 4`
